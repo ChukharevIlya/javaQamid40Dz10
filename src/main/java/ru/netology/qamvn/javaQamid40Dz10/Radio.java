f60950bd607bbf22e12782d614affffc5aae9d28
ChukharevIlya <chukharevowen@yandex.ru>
2022-07-17 18:05:20 +0300

    Initial commit

---
+++ src/main/java/ru/netology/qamvn/javaQamid40Dz10/Radio.java
@@ -0,0 +1,96 @@
+package ru.netology.qamvn.javaQamid40Dz10;
+
+public class Radio {
+
+    public int currentVolume;
+    public int currentRadioStation;
+
+    // Номер текущей радиостанции может принимать значения только в пределах от 0 до 9
+    public int getRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setToMinRadioStation() {
+        currentRadioStation = 0;
+    }
+
+    public void setToMaxRadioStation() {
+        currentRadioStation = 9;
+    }
+
+    public void setRadioStation(int newRadioStation) {
+        if (newRadioStation < 0) {
+            return;
+        }
+        if (newRadioStation > 9) {
+            return;
+        }
+        currentRadioStation = newRadioStation;
+    }
+
+    // Кнопка next
+    public void increaseRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+        }
+        if (currentRadioStation == 9) {
+            currentRadioStation = 0;
+        }
+    }
+
+    // Кнопка prev
+    public void decreaseRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        }
+        if (currentRadioStation == 0) {
+            currentRadioStation = 9;
+        }
+    }
+
+    // выставление любой радиостаници
+    public void setRandomRadioStation(int randomRadioStation) {
+        if (currentRadioStation > 9) {
+            return;
+        }
+        if (currentRadioStation < 0) {
+            return;
+        }
+        currentRadioStation = randomRadioStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    // Клиент должен иметь возможность увеличивать и уменьшать уровень громкости звука (в пределах от 0 до 10)
+    public void setVolume(int newVolume) {
+        if (newVolume < 0) {
+            return;
+        }
+        if (newVolume > 10) {
+            return;
+        }
+        currentVolume = newVolume;
+    }
+
+    // нажатие кнопки "+"
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        if (currentVolume == 10) {
+            currentVolume = 10;
+        }
+    }
+
+    // нажатие кнопки "-"
+    public void decreaseVolume() {
+        if (currentVolume == 0) {
+            currentVolume = 0;
+        }
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/qamvn/javaQamid40Dz10/RadioTest.java
@@ -0,0 +1,120 @@
+package ru.netology.qamvn.javaQamid40Dz10;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shuldSetRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setRadioStation(9);
+
+        int expected = 9;
+        int actual = radio.getRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setRadioStation(12);
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnToMinStationAfterMaxStation() {
+        Radio radio = new Radio();
+
+        radio.setRadioStation(9);
+        radio.increaseRadioStation();
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetToMaxStationAfterMinStation() {
+        Radio radio = new Radio();
+
+        radio.setRadioStation(0);
+        radio.decreaseRadioStation();
+
+        int expected = 9;
+        int actual = radio.getRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetRandomRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setRandomRadioStation(3);
+
+        int expected = 3;
+        int actual = radio.getRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetVolume() {
+        Radio radio = new Radio();
+
+        radio.setVolume(10);
+
+        int expexted = 10;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expexted, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolume() {
+        Radio radio = new Radio();
+
+        radio.setVolume(11);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldStopIncreaseVolumeOn10() {
+        Radio radio = new Radio();
+
+        radio.setVolume(10);
+        radio.increaseVolume();
+
+        int expected = 10;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldStopDecreaseVolumeOn0() {
+        Radio radio = new Radio();
+
+        radio.setVolume(0);
+        radio.decreaseVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
